5240d3de3bb8b6781cb96c5cc18f0beab689018c
hubert.janiuk <kombajndziki@gmai.om>
2024-09-09 14:23:04 +0200

    User Entity

---
+++ event-management-api/src/main/java/pl/jania1857/eventmanagement/user/Role.java
@@ -0,0 +1,6 @@
+package pl.jania1857.eventmanagement.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ event-management-api/src/main/java/pl/jania1857/eventmanagement/user/User.java
@@ -0,0 +1,46 @@
+package pl.jania1857.eventmanagement.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
