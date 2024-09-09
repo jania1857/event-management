1af376dc207a0165efdfe12210484346e48ff19e
hubert.janiuk <kombajndziki@gmai.om>
2024-09-09 12:33:36 +0200

    Initialization of Spring Boot project

---
+++ event-management-api/src/main/java/pl/jania1857/eventmanagement/EventManagementApiApplication.java
@@ -0,0 +1,13 @@
+package pl.jania1857.eventmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagementApiApplication.class, args);
+    }
+
+}
---
+++ event-management-api/src/test/java/pl/jania1857/eventmanagement/EventManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jania1857.eventmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
